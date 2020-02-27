9eb16e0ad22a22c5db928a95dc1fe32b62c22c8b
Dustin Catap <cadn@erni.ph>
2020-02-27 10:31:22 +0800

    Android CI (#3)
    * Update keystore file name.
    
    * Add logging for provided keystore.
    
    * Add app center secrets.
    
    * Fix wrong keystore file extension.

--- src/one_bataan_league_pass_common/lib/src/constants/app_center_constants.dart
+++ src/one_bataan_league_pass_common/lib/src/constants/app_center_constants.dart
@@ -1,4 +1,4 @@
 class AppCenterConstants {
-  static final androidKey = 'xxx';
-  static final iosKey = 'xxx';
+  static final androidKey = 'e32a246b-8026-4e7c-840f-cfeeaa272d53';
+  static final iosKey = 'ad441c76-a04e-4366-b984-5bc9ba83a458';
 }
